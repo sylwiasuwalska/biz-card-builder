24601ed6f8b61196ec33906977360bb060fcee01
sylwiasuwalska@gmail.com <sylwiasuwalska@gmail.com>
2020-03-31 00:37:32 +0200

    Updates

---
+++ precache-manifest.d180dd412e946c5aa1ba03d24fdcb584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc5f5d4646c3f12a8a06fe1a82c748e",
+    "url": "/biz-card-builder/index.html"
+  },
+  {
+    "revision": "437b825e58c025affbaa",
+    "url": "/biz-card-builder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c144cff0be72b69596e0",
+    "url": "/biz-card-builder/static/css/main.8d150754.chunk.css"
+  },
+  {
+    "revision": "437b825e58c025affbaa",
+    "url": "/biz-card-builder/static/js/2.7c2f6245.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/biz-card-builder/static/js/2.7c2f6245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c144cff0be72b69596e0",
+    "url": "/biz-card-builder/static/js/main.1fc9c39a.chunk.js"
+  },
+  {
+    "revision": "6a7fe7ff10b2b75ce2f6",
+    "url": "/biz-card-builder/static/js/runtime-main.1c6af34b.js"
+  }
+]);
