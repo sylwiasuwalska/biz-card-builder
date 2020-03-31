f57562e391fb6724b95a07e3667f19152dff3c66
sylwiasuwalska@gmail.com <sylwiasuwalska@gmail.com>
2020-03-31 14:48:09 +0200

    "added background choice"

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,59 +1,90 @@
-import React, {useContext, useState,} from 'react';
+import React, {useContext, useState} from "react";
 import {Context} from "./Store";
 import {Col, Container, Form, Row} from "react-bootstrap";
-import '../Card.css';
+import "../Card.css";
 import RadioList from "./RadioList";
-
 
 function Card(props) {
     const [globalState, dispatch] = useContext(Context);
     const [alignState, alignSet] = useState("layout-left");
-    const [fontState, fontSet] = useState("font1")
+    const [fontState, fontSet] = useState("font1");
+    const [backgroundState, backgroundSet] = useState("background1");
 
-
-    const handleAlignChange = changeEvent => {
+    const handleAlignChange = (changeEvent) => {
         alignSet(changeEvent.target.value);
     };
 
-    const handleFontChange = changeEvent => {
+    const handleFontChange = (changeEvent) => {
         fontSet(changeEvent.target.value);
-    }
+    };
 
-    const handleFormSubmit = formSubmitEvent => {
+    const handleBackgroundChange = (changeEvent) => {
+        backgroundSet(changeEvent.target.value);
+    };
+
+    const handleFormSubmit = (formSubmitEvent) => {
         formSubmitEvent.preventDefault();
-    }
+    };
 
     return (
-
         <Container className="cardContainer">
             <Row>
-                <div className="box card">
-                    <div className={`layout  ${fontState} ${alignState}`}>
-                        <h3 className="name">{globalState.name}</h3>
-                        <p className="profession">{globalState.profession}</p>
-                        <p className="email">e-mail: {globalState.email}</p>
-                        <p className="telephone">mobile: {globalState.telephone}</p>
-                        <p className="address">addres: {globalState.address}</p>
+                <div className={` box card `}>
+                    <div className={backgroundState}>
+                        <div className={`layout ${fontState} ${alignState}`}>
+                            <h3 className="name">{globalState.name}</h3>
+                            <p className="profession">{globalState.profession}</p>
+                            <p className="email">e-mail: {globalState.email}</p>
+                            <p className="telephone">mobile: {globalState.telephone}</p>
+                            <p className="address">addres: {globalState.address}</p>
+                        </div>
                     </div>
                 </div>
             </Row>
-            <Row>
+            <Row className="contentChanger">
                 <Col>
-                <RadioList title={"Align content:"} handleChange={handleAlignChange}
-                           handleFromSubmit={handleFormSubmit} name={"layout"}
-                           value1={"layout-left"} label1={"Left"}
-                           value2={"layout-center"} label2={"Center"}
-                           value3={"layout-right"} label3={"Right"}/>
+                    <RadioList
+                        title={"Align content:"}
+                        handleChange={handleAlignChange}
+                        handleFromSubmit={handleFormSubmit}
+                        name={"layout"}
+                        value1={"layout-left"}
+                        label1={"Left"}
+                        value2={"layout-center"}
+                        label2={"Center"}
+                        value3={"layout-right"}
+                        label3={"Right"}
+                    />
                 </Col>
                 <Col>
-                <RadioList title={"Choose font:"} handleChange={handleFontChange}
-                           handleFromSubmit={handleFormSubmit} name={"font"}
-                           value1={"font1"} label1={"Roboto"}
-                           value2={"font2"} label2={"Ubuntu"}
-                           value3={"font3"} label3={"Raleway"}/>
+                    <RadioList
+                        title={"Choose font:"}
+                        handleChange={handleFontChange}
+                        handleFromSubmit={handleFormSubmit}
+                        name={"font"}
+                        value1={"font1"}
+                        label1={"Roboto"}
+                        value2={"font2"}
+                        label2={"Ubuntu"}
+                        value3={"font3"}
+                        label3={"Raleway"}
+                    />
+                </Col>
+                <Col>
+                    <RadioList
+                        title={"Choose background:"}
+                        handleChange={handleBackgroundChange}
+                        handleFromSubmit={handleFormSubmit}
+                        name={"background"}
+                        value1={"background1"}
+                        label1={"Background 1"}
+                        value2={"background2"}
+                        label2={"Background 2"}
+                        value3={"background3"}
+                        label3={"Background 3"}
+                    />
                 </Col>
             </Row>
-
         </Container>
     );
 }
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,22 +1,19 @@
 import React, {createContext, useReducer} from "react";
-import Reducer from './Reducer'
-
+import Reducer from "./Reducer";
 
 const initialState = {
     name: "Wojciech Kłos",
     profession: "IT manager",
     email: "wojciech@wojciechklos.com",
     telephone: "+48-607-901-805",
-    address: "Świętego Spokoju 7, Brzeziny"
+    address: "Świętego Spokoju 7, Brzeziny",
 };
 
 const Store = ({children}) => {
     const [state, dispatch] = useReducer(Reducer, initialState);
     return (
-        <Context.Provider value={[state, dispatch]}>
-            {children}
-        </Context.Provider>
-    )
+        <Context.Provider value={[state, dispatch]}>{children}</Context.Provider>
+    );
 };
 
 export const Context = createContext(initialState);
