1ead23d408ae5171335728726a391cca14040326
sylwiasuwalska@gmail.com <sylwiasuwalska@gmail.com>
2020-03-31 13:31:44 +0200

    "added font choice"

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,34 @@
 import React, {useContext, useState,} from 'react';
 import {Context} from "./Store";
-import {Container, Form, FormControl, InputGroup, Row} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 import '../Card.css';
+import RadioList from "./RadioList";
 
 
 function Card(props) {
     const [globalState, dispatch] = useContext(Context);
     const [alignState, alignSet] = useState("layout-left");
+    const [fontState, fontSet] = useState("font1")
 
 
-
-    const handleOptionChange = changeEvent => {
+    const handleAlignChange = changeEvent => {
         alignSet(changeEvent.target.value);
     };
+
+    const handleFontChange = changeEvent => {
+        fontSet(changeEvent.target.value);
+    }
 
     const handleFormSubmit = formSubmitEvent => {
         formSubmitEvent.preventDefault();
     }
-
-
-
 
     return (
 
         <Container className="cardContainer">
             <Row>
                 <div className="box card">
-                    <div className={`layout ${alignState}`}>
+                    <div className={`layout  ${fontState} ${alignState}`}>
                         <h3 className="name">{globalState.name}</h3>
                         <p className="profession">{globalState.profession}</p>
                         <p className="email">e-mail: {globalState.email}</p>
@@ -36,49 +38,20 @@ function Card(props) {
                 </div>
             </Row>
             <Row>
-
-                <Form onSubmit={handleFormSubmit}>
-                    <h4>Align content:</h4>
-                    <div className="form-check">
-                        <label>
-                            <input
-                                type="radio"
-                                name="layout"
-                                value="layout-left"
-                                onChange={handleOptionChange}
-                                className="form-check-input"
-                                defaultChecked
-                            />
-                            Left
-                        </label>
-                    </div>
-
-                    <div className="form-check">
-                        <label>
-                            <input
-                                type="radio"
-                                name="layout"
-                                value="layout-center"
-                                onChange={handleOptionChange}
-                                className="form-check-input"
-                            />
-                            Center
-                        </label>
-                    </div>
-
-                    <div className="form-check">
-                        <label>
-                            <input
-                                type="radio"
-                                name="layout"
-                                value="layout-right"
-                                onChange={handleOptionChange}
-                                className="form-check-input"
-                            />
-                            Right
-                        </label>
-                    </div>
-                </Form>
+                <Col>
+                <RadioList title={"Align content:"} handleChange={handleAlignChange}
+                           handleFromSubmit={handleFormSubmit} name={"layout"}
+                           value1={"layout-left"} label1={"Left"}
+                           value2={"layout-center"} label2={"Center"}
+                           value3={"layout-right"} label3={"Right"}/>
+                </Col>
+                <Col>
+                <RadioList title={"Choose font:"} handleChange={handleFontChange}
+                           handleFromSubmit={handleFormSubmit} name={"font"}
+                           value1={"font1"} label1={"Roboto"}
+                           value2={"font2"} label2={"Ubuntu"}
+                           value3={"font3"} label3={"Raleway"}/>
+                </Col>
             </Row>
 
         </Container>
---
+++ src/components/RadioList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Form} from "react-bootstrap";
+
+function RadioList(props) {
+    return (
+        <Form onSubmit={props.handleFormSubmit}>
+            <h4>{props.title}</h4>
+            <div className="form-check">
+                <label>
+                    <input
+                        type="radio"
+                        name={props.name}
+                        value={props.value1}
+                        onChange={props.handleChange}
+                        className="form-check-input"
+                        defaultChecked
+                    />
+                    {props.label1}
+                </label>
+            </div>
+
+            <div className="form-check">
+                <label>
+                    <input
+                        type="radio"
+                        name={props.name}
+                        value={props.value2}
+                        onChange={props.handleChange}
+                        className="form-check-input"
+                    />
+                    {props.label2}
+                </label>
+            </div>
+
+            <div className="form-check">
+                <label>
+                    <input
+                        type="radio"
+                        name={props.name}
+                        value={props.value3}
+                        onChange={props.handleChange}
+                        className="form-check-input"
+                    />
+                    {props.label3}
+                </label>
+            </div>
+        </Form>
+    );
+}
+
+export default RadioList;
